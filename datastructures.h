9cdede2f6921c0436ba141446350b1fb83768723
Adhitya-Raj <Adhitya-Raj@github.com>
2021-05-26 13:41:37 +0530

    ds.h framework

---
+++ datastructures.h
@@ -0,0 +1,33 @@
+#include <string>
+
+using namespace std;
+
+struct product
+{
+    int productID, quantity;
+    string name;
+    float price;
+    product(int pid,string name, float p=9999.99, int qty=0){
+        productID = pid;
+        this->name = name;
+        price = p;
+        quantity = qty;
+    }
+};
+
+class Store{
+    struct node:product{
+        node *left, *right;
+        node(int pid,string name, float p=9999.99, int qty=0):product(pid,name,p,qty){;}
+    };
+    public:
+
+};
+
+class Cart{
+    struct node:product{
+        node *left, *right;
+        node(int pid,string name, float p=9999.99, int qty=0):product(pid,name,p,qty){;}
+    };
+    public:
+};
